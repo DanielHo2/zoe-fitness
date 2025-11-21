da5f141377db3f37327d21b61f84b179ae9e69f1
hodaniel <ho.daniel248@gmail.com>
2025-11-21 11:58:04 -0800

    initial skeleton file commit

---
+++ src/main/java/ho_daniel/Zoe/Fitness/ZoeFitnessApplication.java
@@ -0,0 +1,13 @@
+package ho_daniel.Zoe.Fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoeFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoeFitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ho_daniel/Zoe/Fitness/ZoeFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package ho_daniel.Zoe.Fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoeFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
