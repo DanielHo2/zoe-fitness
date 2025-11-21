4115f079bc4c6eacdccd6c0f8f7c4c65a604d478
hodaniel <ho.daniel248@gmail.com>
2025-11-21 15:31:05 -0800

    added mongodb database connectivity

--- src/main/java/ho_daniel/Zoe/Fitness/ZoeFitnessApplication.java
+++ src/main/java/ho_daniel/Zoe/Fitness/ZoeFitnessApplication.java
@@ -1,9 +1,15 @@
 package ho_daniel.Zoe.Fitness;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
+// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@RestController
 public class ZoeFitnessApplication {
 
 	public static void main(String[] args) {
